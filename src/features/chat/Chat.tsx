e0213e1b932f7e8dbff6c1ef3176525eca8b5f13
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-04-12 23:30:31 +0800

    chat page done

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import styles from "../../main/ui/header/Header.module.css";
+import React, {useState} from 'react';
+import styles from "./Chat.module.css";
+import SuperInputText from "../../main/ui/common/SuperInputText/SuperInputText";
+import Modal from "../../main/ui/common/Modal/Modal";
+import ModalButtonsWrap from "../../main/ui/common/Modal/ModalButtonsWrap";
+import SuperButton from "../../main/ui/common/SuperButton/SuperButton";
+import chatImg from "./chat2.png"
 
 export const Chat = () => {
+  const [newPackName, setNewPackName] = useState<string>('');
+  const [isModal, setIsModal] = useState<boolean>(false);
+
+  const showModal = () => setIsModal(true);
+  const closeModal = () => setIsModal(false);
+  
+  const sendMassage = () => {
+    
+  }
 
   return (
-    <div>
-      <button className={styles.btnLogout} >Chat</button>
-    </div>
+    <>
+      <div className={styles.chatContainer}>
+        <button className={styles.btnChat} onClick={showModal}>
+          <img src={chatImg} alt="chat" className={styles.chatSymbol}/>
+          &nbsp; Chat
+        </button>
+      </div>
+      <Modal title={'Chat'} show={isModal} closeModal={closeModal}>
+        <div className={styles.chatTable}>
+
+        </div>
+        <label>New massage</label>
+        <SuperInputText value={newPackName} onChangeText={setNewPackName} placeholder={'New massage ...'}/>
+
+        <ModalButtonsWrap closeModal={closeModal}>
+          <SuperButton onClick={sendMassage}>Sand</SuperButton>
+        </ModalButtonsWrap>
+      </Modal>
+    </>
   )
 }
 
