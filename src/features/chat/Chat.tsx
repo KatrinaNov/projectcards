aa7fc2bffd4dc81bfca136ce7aea97806908a089
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-04-12 22:00:57 +0800

    chat start

---
+++ src/features/chat/Chat.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "../../main/ui/header/Header.module.css";
+
+export const Chat = () => {
+
+  return (
+    <div>
+      <button className={styles.btnLogout} >Chat</button>
+    </div>
+  )
+}
+
+
+
